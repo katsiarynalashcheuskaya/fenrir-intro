6eb2422e8141ee8096f3a43820fc9edcd4e5da04
katsiarynalashcheuskaya <ek.sharova@icloud.com>
2023-07-11 10:32:02 -0400

    Added Save Button

--- js/index.js
+++ js/index.js
@@ -5,10 +5,7 @@ const copyright = document.createElement('p');
 copyright.innerHTML = `&#169 Katsiaryna Lashcheuskaya ${thisYear}`;
 footer.appendChild(copyright);
 
-const skills = [
-    'HTML', 'CSS/SCSS', 'JavaScript', 'TypeScript', 'React', 'Redux', 'Redux Toolkit', 'Postman',
-    'Axios', 'REST API', 'Git', 'GitHub', 'Unit-test', 'Storybook', 'Material UI', 'SASS', 'Figma'
-]
+const skills = ['HTML', 'CSS/SCSS', 'JavaScript', 'TypeScript', 'React', 'Redux', 'Redux Toolkit', 'Postman', 'Axios', 'REST API', 'Git', 'GitHub', 'Unit-test', 'Storybook', 'Material UI', 'SASS', 'Figma']
 const skillsSection = document.querySelector('#skills')
 const skillsList = skillsSection.querySelector('ul')
 for (let i = 0; i < skills.length; i++) {
@@ -16,7 +13,6 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
-
 
 const messageForm = document.getElementById('leave_message')
 messageForm.addEventListener('submit', e => {
@@ -36,36 +32,52 @@ messageForm.addEventListener('submit', e => {
     const hideMessageSection = () => {
         messageList.children.length !== 0 ? messageSection.style.display = 'block' : messageSection.style.display = 'none'
     }
-    removeButton.addEventListener('click', e => {
+    const removeFunction = () => {
         const entry = removeButton.parentNode;
         entry.remove();
+    }
+    removeButton.addEventListener('click', e => {
+        removeFunction();
         hideMessageSection();
+    })
+
+    const saveButton = document.createElement('button');
+    saveButton.innerText = 'save';
+    saveButton.type = 'button';
+    saveButton.style.display = 'none'
+    const saveFunction = () => {
+        saveButton.style.display = 'none'
+        editButton.style.display = 'inline';
+        removeButton.style.display = 'inline';
+        let entry = saveButton.parentNode.querySelector('#message');
+        let textarea = document.getElementById('messageTextarea');
+        entry.textContent = textarea.value;
+        textarea.replaceWith(entry);
+    }
+    saveButton.addEventListener('click', e => {
+        saveFunction();
     })
 
     const editButton = document.createElement('button');
     editButton.innerText = 'edit';
     editButton.type = 'button';
-    editButton.addEventListener('click', e => {
-        editButton.innerText = 'save';
+    const editFunction = () => {
+        editButton.style.display = 'none';
         removeButton.style.display = 'none';
-        const textarea = document.createElement('textarea');
+        saveButton.style.display = 'inline'
+        let textarea = document.createElement('textarea');
         textarea.id = 'messageTextarea';
-        const entry = editButton.parentNode.querySelector('#message');
-        console.log('entryEditButton = ', entry);
-        if (entry) {
-            entry.replaceWith(textarea);
-        }
+        let entry = editButton.parentNode.querySelector('#message');
+        entry.replaceWith(textarea);
         textarea.value = entry.innerText;
         textarea.focus();
-        editButton.addEventListener('click', e => {
-            removeButton.style.display = 'inline'
-            editButton.innerText = 'edit';
-            entry.textContent = textarea.value;
-            textarea.replaceWith(entry);
-        })
+    }
+    editButton.addEventListener('click', e => {
+        editFunction();
     })
 
     newMessage.appendChild(editButton);
+    newMessage.appendChild(saveButton);
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
     e.target.reset();
